b99e7363887cbeafc0bfd293b59cb6ed11a39943
Lozen Lin <lozenlin@gmail.com>
2021-11-27 13:36:44 +0800

    增加 Service layer, 建立取得員工資料清單的空殼

---
+++ Source/ForInterviewWork/Common.Services/IEmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Data.Domain.Models;
+
+namespace Common.Services
+{
+    /// <summary>
+    /// 員工管理
+    /// </summary>
+    /// <history>
+    /// 2021/11/27, lozenlin, add
+    /// </history>
+    public interface IEmployeeService
+    {
+        /// <summary>
+        /// 取得資料清單
+        /// </summary>
+        /// <history>
+        /// 2021/11/27, lozenlin, add
+        /// </history>
+        public List<Employee> GetList();
+    }
+}
---
+++ Source/ForInterviewWork/Common.Services/Impl/EmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using Common.Data.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Services.Impl
+{
+    public class EmployeeService : IEmployeeService
+    {
+        /// <summary>
+        /// 員工管理
+        /// </summary>
+        /// <history>
+        /// 2021/11/27, lozenlin, add
+        /// </history>
+        public EmployeeService()
+        {
+
+        }
+
+        /// <summary>
+        /// 取得資料清單
+        /// </summary>
+        /// <history>
+        /// 2021/11/27, lozenlin, add
+        /// </history>
+        public List<Employee> GetList()
+        {
+            List<Employee> entities = null;
+
+
+            return entities;
+        }
+    }
+}
